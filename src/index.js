42e1c349bd6656babfdecfe6fa8432ce99feb11f
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-02-14 13:54:40 -0300

    import the neecessary moduleis to be bundled

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//CSS
+import './assets/css/normalize.css';
+import './assets/css/style.css';
+//JAVASCRIPT
+import './modules/controller';
+
+import './modules/aside.js';
+
+import './modules/toggleTheme.js';
