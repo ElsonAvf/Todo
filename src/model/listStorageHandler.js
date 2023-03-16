12f7bc7824b9fa23fbf01f53a1e8756720b69bbc
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-03-16 14:56:34 -0300

    feat: function that updates the list title

---
+++ src/model/listStorageHandler.js
@@ -0,0 +1,61 @@
+import uniqid from 'uniqid';
+
+import {getMainList, setMainList} from './mainListStorageHandler.js';
+
+export function getListById(listId) {
+  const mainList = getMainList();
+  for (let index = 0; index < mainList.length; index++) {
+    if (mainList[index].id === listId) {
+      return mainList[index];
+    };
+  };
+};
+
+function getListIndex(listId) {
+  const mainList = getMainList();
+  for (let index = 0; index < mainList.length; index++) {
+    if (mainList[index].id === listId) {
+      return index;
+    };
+  };
+};
+
+export function addList(listObj) {
+  if (!listObj.id) {
+    listObj.id = uniqid();
+  };
+  const newList = getMainList();
+  newList.unshift(listObj);
+  setMainList(newList)
+};
+
+function updateList(newListObj, oldIndex) {
+  const mainList = getMainList();
+  mainList.splice(oldIndex, 1, newListObj);
+  setMainList(mainList);
+};
+
+export function updateListTitle(listId, newTitle) {
+  const updatedList = getListById(listId);
+  const oldListIndex = getListIndex(listId);
+  updatedList.title = newTitle;
+  deleteList(listId);
+  updateList(updateList, oldListIndex)
+};
+
+export function deleteList(listId) {
+  const mainList = getMainList();
+  const listIndex = getListIndex(listId);
+  mainList.splice(listIndex, 1)
+  setMainList(mainList);
+};
+
+export function getAllLists() {
+  const mainList = getMainList()
+  let allLists = [];
+  for (let i = 0; i<mainList.length; i++) {
+    const list = getMainList()[i];
+    allLists.push(list);
+  };
+  return allLists;
+};
