1c0c18d9bd7b8defe003ffc886877c21b62a9adb
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-03-16 14:44:14 -0300

    Create Cell component

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import closeSvg from './../assets/icons/red_close.svg';
 
-export default function Cell() {
+import { useDispatchMainContentContext } from './contexts/MainContentContext.js';
+import { deleteCell, updateCell } from './../model/cellStorageHandler.js';
+
+import closeSvg from './../assets/icons/red_close.svg';
+import './../assets/css/Cell.css';
+
+export default function Cell({ cellObj }) {
+  const dispatchMainContent = useDispatchMainContentContext();
+  function deleteSelf() {
+    deleteCell(cellObj.listId, cellObj.id)
+    dispatchMainContent({ type: 'show_cells', id: cellObj.listId})
+  }
+  function handleChange(e) {
+    const { name, checked} = e.target;
+    updateCell(cellObj.listId, cellObj.id, name, checked)
+    dispatchMainContent({ type: 'show_cells', id: cellObj.listId})
+  }
+  
   return (
     <li className='cells'>
-      <input type='checkbox' />
-      <h4></h4>
-      <span><span>
-      <button><img src={closeSvg} alt='remove cell' /></button>
+      <input
+        type='checkbox'
+        name='completed'
+        checked={cellObj.completed}
+        onChange={handleChange}
+      />
+      <h4>{ cellObj.title }</h4>
+      <span>{ cellObj.priority }</span>
+      <button type='button' onClick={deleteSelf}><img src={closeSvg} alt='remove cell' /></button>
     </li>
   );
 };
