24d7f86f50b57850e903faa6517fbde2bec26e3a
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-03-16 17:42:41 -0300

    Create Aside component

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,25 +1,51 @@
 import React from 'react';
 
-import './../assets/css/Aside.css';
+import AsideList from './AsideList';
 
+import { useThemeContext } from './contexts/ThemeContext.js';
+import { useDispatchMainContentContext } from './contexts/MainContentContext.js';
+import { useDispatchShowListContext } from './contexts/ShowListContext.js';
+
+import { getAllLists } from './../model/listStorageHandler.js';
+
+import './../assets/css/Aside.css';
 import closeSvg from './../assets/icons/close.svg'
 
-import {getAllLists} from './../model/localStorageHandler.js';
-
-export default function Aside({darkMode, toggleAside}) {
-  const [groups, setGroups] = React.useState(getAllLists())
-   console.log('hi')
-   let styles = {
-     backgroundColor: darkMode ? '#2D60B9' : '#0D1C36',
-     color: darkMode ? 'white' : 'black',
+export default function Aside({ toggleAside }) {
+  const dispatchMainContent = useDispatchMainContentContext();
+  const dispatchShowList = useDispatchShowListContext();
+  const theme = useThemeContext();
+  const groups = getAllLists();
+  
+  function change() {
+    dispatchShowList(true);
+    dispatchMainContent({ type: 'show_lists'});
+  };
+       
+   const styles = {
+     backgroundColor: theme ? '#1A386C' : '#0D1C36',
+     color: theme ? 'white' : 'black',
    }
   return (
     <aside style={styles}>
       <button id='close' onClick={toggleAside}><img src={closeSvg} /></button>
       <button id='home-button' type='button'>Home</button>
-      <button id='list-button' type='button'>Lists</button>
+      <button
+        onClick={change}
+        id='list-button'
+        type='button'
+      >
+        Lists
+      </button>
       <ul>
-        {groups.map(group => <li><button>{group.title}</button></li>)}
+        {groups.map(group =>
+          <AsideList
+            key={group.id}
+            listid={group.id}
+            title={group.title}
+           />
+          )
+        }
       </ul>
     </aside>
   );
