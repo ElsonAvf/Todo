f974c067ad27f9fb025d869273d0d7a8fc01dcea
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-03-16 18:07:47 -0300

    Create MainContent context
    this defines what the main component should display
    wheter cell or lists
    different than ShowList component, this context doesntdefines what
    component the MainList should display, instead, this defines what
    content the MainList will display

---
+++ src/components/contexts/MainContentContext.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { getAllLists, getListById } from './../../model/listStorageHandler.js';
+
+const MainContentContext = React.createContext(null);
+const DispatchMainContentContext = React.createContext(null);
+
+export function useMainContentContext() {
+  return React.useContext(MainContentContext);
+};
+export function useDispatchMainContentContext() {
+  return React.useContext(DispatchMainContentContext);
+};
+
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'show_lists':
+      return { title: 'Lists', content: getAllLists() };
+    case 'show_cells':
+      const list = getListById(action.id);
+      return { title: list.title, content: list.cellList };
+  }
+};
+function init() {
+  return { title: 'Lists', content: getAllLists() };
+}
+
+export function MainContentProvider({ children }) {
+  const [mainContent, dispatchMainContent] = React.useReducer(reducer, null, init) 
+  return (
+    <MainContentContext.Provider value={mainContent}>
+      <DispatchMainContentContext.Provider value={dispatchMainContent}>
+        { children }
+      </DispatchMainContentContext.Provider>
+    </MainContentContext.Provider>
+  )
+}
