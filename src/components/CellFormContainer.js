5536e4b795394b9c540e34c9f96714bc4e79e76f
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-03-17 17:54:26 -0300

    Update

--- src/components/CellFormContainer.js
+++ src/components/CellFormContainer.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import CellForm from './CellForm';
-import { CellFormProvider } from './contexts/CellFormContext';
 
 import './../assets/css/CellForm.css'
 
 export default function CellFormContainer({ toggleForm, listId }) {
   return (
     <div id='cell-form-container'>
-    <CellFormProvider>
       <CellForm toggleForm={toggleForm} listId={listId} />
-    </CellFormProvider>
     </div>
   );
 };
