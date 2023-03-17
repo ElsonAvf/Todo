8bb42f7c9c917dff1881bc4f1f156c6aec878f31
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-03-17 19:05:27 -0300

    Replace svg with mdi menu icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { useThemeContext, useDispatchThemeContext  } from './contexts/ThemeContext.js'
 
 import './../assets/css/Header.css';
-
+import Icon from '@mdi/react';
+import { mdiMenu } from '@mdi/js'
 import darkModeSvg from './../assets/icons/dark_mode.svg';
 import lightModeSvg from './../assets/icons/light_mode.svg';
 
@@ -19,9 +20,7 @@ export default function Header({ toggleMode, toggleAside }) {
     <header>
       <h1 style={{color: colorTheme}} id='logo'>Todo</h1>
       <button onClick={toggleAside} id='menu-bar' type='button' >
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill={colorTheme}>
-          <path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
-        </svg>
+        <Icon path={mdiMenu} size={1} color={colorTheme} />
       </button>
       <input style={searchInputStyle} type='search' />
       <button onClick={dispatchTheme} type='button' id='theme-icon'>
