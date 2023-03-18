b8dd532c14012b0e2fab2f92c70bd4b3ce406722
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-03-18 19:44:31 -0300

    Create main list if it doesnt exist

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './components/App';
 import {ThemeProvider} from './components/contexts/ThemeContext.js';
+import {ListIdProvider} from './components/contexts/ListIdContext.js';
 import {MainContentProvider} from './components/contexts/MainContentContext.js';
 //CSS
 import './assets/css/normalize.css';
 import './assets/css/style.css';
 //JAVASCRIPT
+if (!localStorage.length) {
+  localStorage.setItem('Lists', JSON.stringify([]))
+}
 const root = createRoot(document.getElementById('root'));
 root.render(
   <MainContentProvider>
     <ThemeProvider>
-      <App />
+      <ListIdProvider>
+        <App />
+      </ListIdProvider>
     </ThemeProvider>
   </MainContentProvider>
 );
