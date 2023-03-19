074def82ee72c213a14f766dab2a25d346255549
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-03-19 14:21:39 -0300

    Use list id context
    when cancel button is clicked this change the list id to null
    if listId exists the initial title get the list title from data and use
    it in the input

--- src/components/ListForm.js
+++ src/components/ListForm.js
@@ -1,39 +1,52 @@
 import React from 'react';
 
 import { useDispatchMainContentContext } from './contexts/MainContentContext.js';
-
-import { addList } from './../model/listStorageHandler.js';
+import { useListIdContext, useDispatchListIdContext } from './contexts/ListIdContext.js';
+import { useTypeOfSubmitContext } from './contexts/TypeOfSubmitContext.js';
+import { addList, updateListTitle, getListById } from './../model/listStorageHandler.js';
 import List from './../model/list';
 
 import './../assets/css/ListForm.css';
 
 export default function ListForm({ toggleForm }) {
-  const [list, setList] = React.useState({title: ''})
+  const listId = useListIdContext();
+  const dispatchListId = useDispatchListIdContext();
+  const initialTitle = listId ? getListById(listId).title : ''
+  const [title, setTitle] = React.useState(initialTitle);
   const changeMainContent = useDispatchMainContentContext();
+  const typeOfSubmit = useTypeOfSubmitContext();
   
   function handleChange(e) {
-    setList({title: e.target.value})
+    setTitle(e.target.value)
   }
-  function addNewList(e) {
-    e.preventDefault();
-    addList(new List(list.title))
-    changeMainContent({ type: 'show_lists'} )
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (typeOfSubmit === 'add_new') {
+      addList(new List(title))
+    } else if (typeOfSubmit === 'edit') {
+      updateListTitle(listId, title)
+    }
+    changeMainContent({ type: 'show_lists'})
     toggleForm();
+  }
+  function cancel() {
+    dispatchListId(null);
+    toggleForm()
   }
   
   return (
     <div id='list-form-container'>
-      <form id='list-form' onSubmit={addNewList}>
+      <form id='list-form' onSubmit={handleSubmit}>
         <input
           onChange={handleChange}
-          value={list.title}
+          value={title}
           placeholder='Title'
           type='text'
           minLength='3'
           maxLength='20'
           required
         />
-        <button type='button' onClick={toggleForm}>Cancel</button>
+        <button type='button' onClick={cancel}>Cancel</button>
         <button type='submit'>Ok</button>
       </form>
     </div>
