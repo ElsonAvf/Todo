383617fb2b5cf97c9635e0034ef45f093d214253
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-03-19 23:35:37 -0300

    Handle search input change
    everytime the input changes it will search for a list that match the
    input

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useThemeContext, useDispatchThemeContext  } from './contexts/ThemeContext.js'
+
+import { useThemeContext, useDispatchThemeContext  } from './contexts/ThemeContext.js';
+import { useDispatchMainContentContext } from './contexts/MainContentContext.js';
+import { useListIdContext } from './contexts/ListIdContext.js';
 
 import './../assets/css/Header.css';
 import Icon from '@mdi/react';
@@ -9,7 +12,18 @@ import lightModeSvg from './../assets/icons/light_mode.svg';
 
 export default function Header({ toggleMode, toggleAside }) {
   const theme = useThemeContext();
+  const listId = useListIdContext();
   const dispatchTheme = useDispatchThemeContext();
+  const dispatchMainContent = useDispatchMainContentContext();
+  const [search, setSearch] = React.useState('')
+  
+  React.useEffect(() => {
+    dispatchMainContent({type: 'show_searched_lists', searchValue: search})
+  }, [search])
+  function handleChange(e) {
+    setSearch(e.target.value)
+  }
+  
   let colorTheme = theme ? 'white' : '#0d1c36';
   let searchInputStyle = {
     backgroundColor: theme ? '#222222' : 'white',
@@ -22,7 +36,7 @@ export default function Header({ toggleMode, toggleAside }) {
       <button onClick={toggleAside} id='menu-bar' type='button' >
         <Icon path={mdiMenu} size={1} color={colorTheme} />
       </button>
-      <input style={searchInputStyle} type='search' />
+      <input style={searchInputStyle} type='search' value={search} onChange={handleChange}/>
       <button onClick={dispatchTheme} type='button' id='theme-icon'>
         <img src={theme ? lightModeSvg : darkModeSvg}/>
       </button>
