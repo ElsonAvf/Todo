e880a435f3aa7806ed298667db6487d8bed3c9c8
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-03-21 14:05:22 -0300

    Remove Main complexity

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,74 +1,16 @@
 import React from 'react';
-import List from './List';
-import Cell from './Cell';
-import ListForm from './ListForm';
-import CellFormContainer from './CellFormContainer';
 
-import { useMainContentContext } from './contexts/MainContentContext.js';
-import { useShowListContext } from './contexts/ShowListContext.js';
-import { useDispatchCellContext } from './contexts/CellFormContext.js';
-import { useDispatchTypeOfSubmitContext } from './contexts/TypeOfSubmitContext.js';
-import { useThemeContext } from './contexts/ThemeContext.js';
+import { ToggleFormProvider } from './contexts/ToggleFormContext.js';
+import { useDisplayContext } from './contexts/DisplayContext.js';
 
 import './../assets/css/Main.css';
 
-import Icon from '@mdi/react';
-import { mdiPlus } from '@mdi/js';
-
 export default function Main() {
-  const mainContent = useMainContentContext();
-  const showList = useShowListContext();
-  const theme = useThemeContext();
-  const dispatchCell = useDispatchCellContext()
-  const dispatchTypeOfSubmit = useDispatchTypeOfSubmitContext()
-  const [showForm, setShowForm] = React.useState(false);
+  const display = useDisplayContext()
   
-  function isEmpty() {
-    return (mainContent.content.length > 0) ? false : true;
-  }
-  
-  function toggleForm() {
-    setShowForm(prevShowForm => !prevShowForm);
-  };
-  
-  function displayList(listObj) {
-    return (<List key={listObj.id} listObj={listObj} toggleForm={toggleForm}/>)
-  };
-  function displayCell(cellObj) {
-    return (<Cell key={cellObj.id} cellObj={cellObj} toggleForm={toggleForm} />);
-  };
-  function displayListForm() {
-    return (<ListForm toggleForm={toggleForm} />)
-  }
-  function displayCellForm() {
-    return (<CellFormContainer toggleForm={toggleForm}  />)
-  }
-  
-  const display = showList ? displayList : displayCell;
-  const form = showList ? displayListForm() : displayCellForm();
-  let content = mainContent.content.map(obj => display(obj))
-  let colorTheme = theme ? 'white' : '#0d1c36';
   return (
-    <main>
-      <h2 style={{color: colorTheme}}>{mainContent.title}</h2>
-      <ul id='menu-container'>
-       { isEmpty() ? <div>Empty</div> : content }
-      </ul>
-      <button 
-        id='add-new'
-        style={
-          {backgroundColor: theme ? '#1A386C' : '#0D1C36'}
-        }
-        onClick={() => {
-          dispatchCell({ type: 'reset' })
-          dispatchTypeOfSubmit({ type: 'add_new' })
-          toggleForm()
-          }
-        }
-      >
-        <Icon className='mdiPlus' path={mdiPlus} size={1} color={'white'}/>
-      </button>
-      {showForm && form}
-    </main>
+    <ToggleFormProvider>
+      {display}
+    </ToggleFormProvider>
   );
 };
