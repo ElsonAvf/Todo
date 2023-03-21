4dc37a46412fa00fade0a3f0968055a9c58fe5fa
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-03-21 15:38:52 -0300

    Use ToggleForm dispatch

--- src/components/ListForm.js
+++ src/components/ListForm.js
@@ -3,13 +3,16 @@ import React from 'react';
 import { useDispatchMainContentContext } from './contexts/MainContentContext.js';
 import { useListIdContext, useDispatchListIdContext } from './contexts/ListIdContext.js';
 import { useTypeOfSubmitContext } from './contexts/TypeOfSubmitContext.js';
+import { useDispatchToggleFormContext } from './contexts/ToggleFormContext.js';
+
 import { addList, updateListTitle, getListById } from './../model/listStorageHandler.js';
 import List from './../model/list';
 
 import './../assets/css/ListForm.css';
 
-export default function ListForm({ toggleForm }) {
+export default function ListForm() {
   const listId = useListIdContext();
+  const toggleForm = useDispatchToggleFormContext();
   const dispatchListId = useDispatchListIdContext();
   const initialTitle = listId ? getListById(listId).title : ''
   const [title, setTitle] = React.useState(initialTitle);
@@ -25,6 +28,7 @@ export default function ListForm({ toggleForm }) {
       addList(new List(title))
     } else if (typeOfSubmit === 'edit') {
       updateListTitle(listId, title)
+      dispatchListId(null);
     }
     changeMainContent({ type: 'show_lists'})
     toggleForm();
