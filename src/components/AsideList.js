2e16140f5f803971efffd2eacb5bdbc940547bdc
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-03-21 14:10:19 -0300

    Use TypeOfDisplay context

--- src/components/AsideList.js
+++ src/components/AsideList.js
@@ -3,19 +3,26 @@ import React from 'react';
 import { useDispatchMainContentContext } from './contexts/MainContentContext.js';
 import { useDispatchListIdContext } from './contexts/ListIdContext.js';
 import { useDispatchShowListContext } from './contexts/ShowListContext.js';
+import { useDispatchDisplayContext } from './contexts/DisplayContext.js'
+import { useDispatchTypeOfDisplayContext } from './contexts/TypeOfDisplayContext.js';
 
 export default function AsideList({ listId, title }) {
   const dispatchMainContent = useDispatchMainContentContext();
+  const dispatchDisplay = useDispatchDisplayContext();
   const dispatchListId = useDispatchListIdContext();
+  const dispatchTypeOfDisplay = useDispatchTypeOfDisplayContext();
   const dispatchShowList = useDispatchShowListContext();
-  function change() {
+  
+  function displayCells() {
     dispatchShowList(false);
-    dispatchListId(listId)
+    dispatchListId(listId);
+    dispatchDisplay({ type: 'cells' })
     dispatchMainContent({ type: 'show_cells', id: listId });
+    dispatchTypeOfDisplay('cells')
   }
   return (
     <li listid={listId}>
-      <button onClick={change}
+      <button onClick={displayCells}
       >
         {title}
       </button>
