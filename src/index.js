8721764027b2f8609202c602718add0399814c83
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-03-21 13:54:04 -0300

    Use TypeOfDisplay context provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
+
 import App from './components/App';
+
 import {ThemeProvider} from './components/contexts/ThemeContext.js';
 import {ListIdProvider} from './components/contexts/ListIdContext.js';
 import {MainContentProvider} from './components/contexts/MainContentContext.js';
+import { TypeOfDisplayProvider } from './components/contexts/TypeOfDisplayContext.js';
+import {DisplayProvider} from './components/contexts/DisplayContext.js'
 //CSS
 import './assets/css/normalize.css';
 import './assets/css/style.css';
@@ -14,10 +18,14 @@ if (!localStorage.length) {
 const root = createRoot(document.getElementById('root'));
 root.render(
   <MainContentProvider>
-    <ThemeProvider>
-      <ListIdProvider>
-        <App />
-      </ListIdProvider>
-    </ThemeProvider>
+    <TypeOfDisplayProvider>
+      <DisplayProvider>
+        <ThemeProvider>
+          <ListIdProvider>
+            <App />
+          </ListIdProvider>
+        </ThemeProvider>
+      </DisplayProvider>
+    </TypeOfDisplayProvider>
   </MainContentProvider>
 );
